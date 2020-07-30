19c50a56af486f5998d37399484deeca8816b0f7
Plaksin Aleksander <plaksin93ao@gmail.com>
2020-07-30 13:11:23 +0300

    init db

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-basedir = os.path.abcpath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
